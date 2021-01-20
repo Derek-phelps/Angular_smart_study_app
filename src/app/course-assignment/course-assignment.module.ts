18f610a6875b00b46b181e8c6fd86b4ea5ae0531
d-gruber <daniel.gruber@smart-study.at>
2021-01-20 11:49:33 +0100

    Removed commented out mat modules.

--- src/app/course-assignment/course-assignment.module.ts
+++ src/app/course-assignment/course-assignment.module.ts
@@ -3,14 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { AppTranslationModule } from '../app.translation.module';
 
-// import { MatRadioModule } from '@angular/material/radio';
-// import { MatSelectModule } from '@angular/material/select';
-// import { MatDatepickerModule } from '@angular/material/datepicker';
-// import { MatNativeDateModule } from '@angular/material';
-// import { MatFormFieldModule, MatInputModule } from '@angular/material';
-// import { MatCheckboxModule } from '@angular/material/checkbox';
-// import { MatTooltipModule } from '@angular/material/tooltip';
-
 import { AppMaterialModule } from '../app-material/app-material.module';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -23,14 +15,6 @@ import { CourseAssignmentComponent } from './course-assignment.component';
   imports: [
     CommonModule,
     AppTranslationModule,
-    // MatRadioModule,
-    // MatSelectModule,
-    // MatDatepickerModule,
-    // MatNativeDateModule,
-    // MatFormFieldModule,
-    // MatInputModule,
-    // MatCheckboxModule,
-    // MatTooltipModule,
     FormsModule,
     ReactiveFormsModule,
     AppMaterialModule
